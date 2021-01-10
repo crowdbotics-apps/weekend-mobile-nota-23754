44b07fd3f738786aaf91d3d682aa26f3e1f04759
Crowdbotics <team@crowdbotics.com>
2021-01-10 19:14:32 +0000

    Initial commit.

---
+++ ios/weekend_mobile_nota_23754Tests/weekend_mobile_nota_23754Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface weekend_mobile_nota_23754 : XCTestCase
+
+@end
+
+@implementation weekend_mobile_nota_23754
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
